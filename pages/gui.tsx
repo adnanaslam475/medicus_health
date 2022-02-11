8b45448548e35e5d7fd71d430ebc0d685159d1b4
Imtiaz Ahmed <imtiaz.ahmed@cooperativecomputing.com>
2022-02-11 20:49:32 +0500

    Ant design alert and dropdown theme modification as per ui guide

--- pages/gui.tsx
+++ pages/gui.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Container from "../src/common/components/Container/Container";
-import { Button, Row, Col, Tag, Radio } from "antd";
+import {
+  Button,
+  Row,
+  Col,
+  Tag,
+  Radio,
+  Checkbox,
+  Form,
+  Input,
+  Alert,
+  Select,
+  DatePicker,
+} from "antd";
 import { PlusCircleFilled } from "@ant-design/icons";
+
+const { Option } = Select;
 
 const GUI = () => {
   return (
     <Container>
+      <h3 className="pt-20">02. TYPOGRAPHY</h3>
+      <hr />
       <h1>H1 Heading Create your account to start using Medicus</h1>
       <h2>H2 Heading</h2>
       <h3>H3 Heading</h3>
@@ -31,7 +47,6 @@ const GUI = () => {
           </Button>
         </Col>
       </Row>
-
       <Row className="pt-5">
         <Col span={6}>
           <Button type="default">Primary Button</Button>
@@ -58,6 +73,21 @@ const GUI = () => {
         <Tag color="cyan">Confirm</Tag>
       </Row>
 
+      <h3 className="pt-20">07. DROPDOWNS</h3>
+      <hr />
+      <Row className="pt-5 pb-20">
+        <Col span={6}>
+          <Select style={{ width: 200 }} placeholder="Dropdown">
+            <Option value="1">Text 1</Option>
+            <Option value="2">Text 2</Option>
+            <Option value="3">Text 3</Option>
+          </Select>
+        </Col>
+        <Col span={6}>
+          <DatePicker />
+        </Col>
+      </Row>
+
       <h3 className="pt-20">05. FORMS</h3>
       <hr />
       <Row className="pt-5">
@@ -70,6 +100,47 @@ const GUI = () => {
           </Radio.Group>
         </Col>
       </Row>
+      <Row className="pt-5">
+        <Col span={6}>
+          <Checkbox>Checkbox</Checkbox>
+        </Col>
+        <Col span={6}>
+          <Checkbox checked>Checkbox</Checkbox>
+        </Col>
+      </Row>
+      <Row className="pt-5">
+        <Col span={6}>
+          <Form.Item name="name">
+            <Input placeholder="Placeholder" />
+          </Form.Item>
+        </Col>
+      </Row>
+      <h3 className="pt-20">06. ALERT BOXS</h3>
+      <hr />
+      <Col className="pt-10" span={12}>
+        <Alert
+          message="Well done! You successfully read the important alert message"
+          type="success"
+        />
+      </Col>
+      <Col className="pt-5" span={12}>
+        <Alert
+          message="Heads up! This alert needs your attention, but it's not super imporant"
+          type="info"
+        />
+      </Col>
+      <Col className="pt-5" span={12}>
+        <Alert
+          message="Warning! Better check yourself, you're not looking too good"
+          type="warning"
+        />
+      </Col>
+      <Col className="pt-5" span={12}>
+        <Alert
+          message="Oh snap! Change a few things up and try submitting again"
+          type="error"
+        />
+      </Col>
     </Container>
   );
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -38,12 +38,14 @@ const Login = () => {
       <div className="flex items-center justify-center min-h-screen w-h-100 py-16">
         <div className="w-full sm:w-1/3 md:w-1/2 lg:w-1/2 xl:w-1/2 px-0">
           <div className="card p-4 shadow-lg drop-shadow-2xl rounded-lg bg-white pt-12 pb-6 px-6">
-          <div className="flex justify-center mb-6"><Image
-              className="main-logo mx-auto"
-              height={34}
-              width={216}
-              src="/assets/images/logo-medi.svg"
-            /></div>
+            <div className="flex justify-center mb-6">
+              <Image
+                className="main-logo mx-auto"
+                height={34}
+                width={216}
+                src="/assets/images/logo-medi.svg"
+              />
+            </div>
             <h1 className="text-center text-secondary mb-3">
               Login to continue
             </h1>
@@ -51,66 +53,65 @@ const Login = () => {
               Enter your credentials to access your account.
             </h5>
             <div className="mt-10">
-            <Form
-              layout="vertical"
-              name="basic"
-              initialValues={{ remember: true }}
-              onFinish={onFinish}
-              onFinishFailed={onFinishFailed}
-              autoComplete="off"
-            >
-              <Form.Item
-                label="Email Address"
-                name="email"
-                className="mb-1"
-                rules={[
-                  {
-                    required: false,
-                    message: "Please enter your email address",
-                  },
-                  {
-                    type: "email",
-                    message: "Email is invalid",
-                  },
-                ]}
+              <Form
+                layout="vertical"
+                name="basic"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
               >
-                <Input size="large" />
-              </Form.Item>
-
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[
-                  { required: false, message: "Please input your password!" },
-                ]}
-              >
-                <Input.Password size="large" />
-              </Form.Item>
-
-              <Form.Item name="remember" valuePropName="checked">
-                <div className="flex justify-between text-base">
-                <Checkbox className="text-base">
-                  Remember me
-                </Checkbox>
-                <Link href="/signup">Forgot Password</Link>
-                </div>
-              </Form.Item>
-
-              <Form.Item>
-                <Button
-
-                  className="ant-btn ant-btn-secondary ant-btn-block nb-button"
-                  type="primary"
-                  htmlType="submit"
+                <Form.Item
+                  label="Email Address"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter your email address",
+                    },
+                    {
+                      type: "email",
+                      message: "Email is invalid",
+                    },
+                  ]}
                 >
-                  Login
-                </Button>
-              </Form.Item>
-            </Form>
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    { required: true, message: "Please input your password!" },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+
+                <Form.Item name="remember" valuePropName="checked">
+                  <div className="flex justify-between text-base">
+                    <Checkbox className="text-base">Remember me</Checkbox>
+                    <Link href="/signup">Forgot Password</Link>
+                  </div>
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    className="ant-btn ant-btn-secondary ant-btn-block nb-button"
+                    type="primary"
+                    htmlType="submit"
+                  >
+                    Login
+                  </Button>
+                </Form.Item>
+              </Form>
             </div>
             <Form.Item>
               <div className="flex justify-center mt-8">
-              Dont have an account? <span className="ml-2"><Link href="/signup">Register</Link></span>
+                Dont have an account?{" "}
+                <span className="ml-2">
+                  <Link href="/signup">Register</Link>
+                </span>
               </div>
             </Form.Item>
           </div>
