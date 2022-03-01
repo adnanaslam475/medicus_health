429cf3f6d0e96729161a71cc8c0d5e17c98a8147
ali <ali.imtiaz@cooperativecomputing.com>
2022-03-01 19:26:13 +0500

    ui fixes buttons added on profile menu UI imporved

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,9 +1,4 @@
 import React from "react";
-import Link from "next/link";
-import { Form, Button, } from "antd";
-import Container from "../src/common/components/Container/Container";
-import Image from "next/image";
-// import  AuthLayout from "../src/common/components/AuthLayout/AuthLayout";
 import AppLayout from "../src/common/components/Layout";
 import AccountTabs from "../src/common/components/Account_Tabs"
 
