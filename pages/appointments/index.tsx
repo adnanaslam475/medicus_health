3bd862cfae910b81da044be8171dfe082960f10f
ali <ali.imtiaz@cooperativecomputing.com>
2022-03-02 16:46:03 +0500

    folder structure upadted for menus and sub menus

---
+++ pages/appointments/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Appointments from "../../src/modules/admin/pages/appointments/AppointmentDetail/appointmentDetail";
+
+
+function appointments() {
+  return <Appointments />;
+}
+
+export default appointments;
+
+
+
---
+++ src/modules/admin/pages/appointments/UpcomingAppointment/UpcomingAppointments.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AppLayout from "../../../../../common/components/Layout";
+
+
+function UpcomingAppointments() {
+    return (
+        <AppLayout>
+            <div className="w-full py-5">
+                <h2 className="mb-3">Upcoming Appointments</h2>
+                <div className="w-full">
+                    {/* <AccountTabs></AccountTabs> */}
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
+export default UpcomingAppointments;
