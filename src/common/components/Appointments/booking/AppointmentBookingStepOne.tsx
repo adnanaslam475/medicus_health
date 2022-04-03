7ca5a3efb2c3d90f1c961a2c7bc4603ea9b0c35e
ali <ali.imtiaz@cooperativecomputing.com>
2022-04-03 14:57:22 +0500

    build issue resolved in UI-structure

--- src/common/components/Appointments/booking/AppointmentBookingStepOne.tsx
+++ src/common/components/Appointments/booking/AppointmentBookingStepOne.tsx
@@ -1,48 +1,50 @@
 import React from "react";
-import { Form, Input, Button, Select, DatePicker} from "antd";
+import { Form, Input, Button, Select, DatePicker } from "antd";
+
+const { Option } = Select;
 
 function StepOne() {
   return (
     <>
-    <Form layout="vertical">
+      <Form layout="vertical">
         <Form.Item label="Doctor*">
-            <Select disabled placeholder="Dr. Paul Wallner" className="w-full">
-                <Option value="Dr.Paul Wallner">Dr.Paul Wallner</Option>
-            </Select>
+          <Select disabled placeholder="Dr. Paul Wallner" className="w-full">
+            <Option value="Dr.Paul Wallner">Dr.Paul Wallner</Option>
+          </Select>
         </Form.Item>
         <div className="flex">
-            <div className="w-5/6">
-                <Form.Item label="Service*">
-                    <Select placeholder="Dr. Paul Wallner" className="w-full">
-                        <Option value="Dr.Paul Wallner">Dr.Paul Wallner</Option>
-                    </Select>
-                </Form.Item>
-            </div>
-            <div className="w-1/6 ml-4">
-                <Form.Item label="Charges">
-                    <div className="text-primary bg-gray-6 rounded flex items-center	justify-center h-12 w-full">
-                        $59.00
-                    </div>
-                </Form.Item>
-            </div>
+          <div className="w-5/6">
+            <Form.Item label="Service*">
+              <Select placeholder="Dr. Paul Wallner" className="w-full">
+                <Option value="Dr.Paul Wallner">Dr.Paul Wallner</Option>
+              </Select>
+            </Form.Item>
+          </div>
+          <div className="w-1/6 ml-4">
+            <Form.Item label="Charges">
+              <div className="text-primary bg-gray-6 rounded flex items-center	justify-center h-12 w-full">
+                $59.00
+              </div>
+            </Form.Item>
+          </div>
         </div>
         <Form.Item label="Doctor*">
-            { <DatePicker className="w-full" /> }
+          {<DatePicker className="w-full" />}
         </Form.Item>
         <Form.Item label="Avaiability*">
-            <div className="flex flex-wrap">
+          <div className="flex flex-wrap">
             <div className="w-44 bg-gray-4 rounded flex items-center justify-center h-8 w-full mr-3 mb-3">
-                    07:00 am - 09:00 am
-                </div>
-                <div className="w-44 bg-cyan text-white rounded flex items-center justify-center h-8 w-full mr-3 mb-3">
-                    07:00 am - 09:00 am
-                </div>
-                <div className="w-44 bg-gray-4 rounded flex items-center justify-center h-8 w-full mr-3 mb-3">
-                    07:00 am - 09:00 am
-                </div>
+              07:00 am - 09:00 am
             </div>
+            <div className="w-44 bg-cyan text-white rounded flex items-center justify-center h-8 w-full mr-3 mb-3">
+              07:00 am - 09:00 am
+            </div>
+            <div className="w-44 bg-gray-4 rounded flex items-center justify-center h-8 w-full mr-3 mb-3">
+              07:00 am - 09:00 am
+            </div>
+          </div>
         </Form.Item>
-    </Form>
+      </Form>
     </>
   );
 }
