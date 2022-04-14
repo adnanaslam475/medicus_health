769b94fedafccfecc9a8fa110c237a7862467024
usama khan <usama.khan@cooperativecomputing.com>
2022-04-15 01:11:50 +0500

    set schedule ui

--- src/modules/patient/components/MakePayment/MakePayment.tsx
+++ src/modules/patient/components/MakePayment/MakePayment.tsx
@@ -1,29 +1,25 @@
-import { Radio } from 'antd'
-import Image from 'next/image';
-import React from 'react';
-import visa from './../../../../../public/assets/images/visa.svg'
+import { Button, Radio } from "antd";
+import Image from "next/image";
+import React from "react";
+import Payment from "../Payment/Payment";
+import visa from "./../../../../../public/assets/images/visa.svg";
+import mastercard from "./../../../../../public/assets/images/mastercard.svg";
+import { PlusOutlined } from "@ant-design/icons";
+import _Classes from './MakePayment.module.scss';
 
 function MakePayment() {
-  return (
-    <div >
-        <div className='bg-gray-4 flex items-center py-1'>
-            <Radio>aaa</Radio>
-            <div className="bg-white rounded px-2">
-            <Image
-                  alt=""
-                  src={visa}
-                  width={46}
-                  height={34}
-                  className="border rounded border-gray-2"
-                />
-</div>
-<div>
-    <h4>Visa Ending with 2256</h4>
-    <p>Expires: 05/2026</p>
-</div>
-        </div>
+	return (
+		<div>
+			<Payment visa={visa}/>
+            
+            <Payment visa={mastercard}/>
+            <div className="flex items-center"><PlusOutlined className={`${_Classes["icon-color"]}`}/> <span className="text-primary">Ad Payment Method</span></div>
+	<div className="flex justify-end">
+        <Button type="primary" className={`${_Classes["button-color"]}`}>Pay $5900</Button>
+        
     </div>
-  )
+    	</div>
+	);
 }
 
-export default MakePayment
+export default MakePayment;
