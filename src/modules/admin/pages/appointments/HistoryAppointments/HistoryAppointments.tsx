a64f2e1f8148285e32878c4e650d42f574c65837
ali <ali.imtiaz@cooperativecomputing.com>
2022-04-21 04:08:16 +0500

    Build Issue Fix

--- src/modules/admin/pages/appointments/HistoryAppointments/HistoryAppointments.tsx
+++ src/modules/admin/pages/appointments/HistoryAppointments/HistoryAppointments.tsx
@@ -1,10 +1,11 @@
-import { Select, DatePicker, Space, Button } from "antd";
+import { Select, DatePicker, Space, Button, Empty } from "antd";
 import React, { useState } from "react";
 import AppLayout from "../../../../../common/components/AppLayout/AppLayout";
 import { CloseOutlined } from "@ant-design/icons";
 import TransactionHistory from "../../../../../common/components/AccountTabs/TransactionHistory/TransactionHistory";
 import SearchFilters from "../../../../../common/components/SearchFilters/SearchFilters";
 import { useGetAllRequestedAppointmentsQuery } from "../../../../../generated/graphql";
+import AppointmentCard from "../../../../../common/components/AppointmentCard/AppointmentCard";
 
 const { RangePicker } = DatePicker;
 
@@ -18,7 +19,7 @@ function CancelledAppointment() {
   const [{ data }] = useGetAllRequestedAppointmentsQuery({
     variables: {
       filter: {
-        status: "Cancelled",
+        status: "Requested",
         physicianName: dataListPhysician,
         doctorId: doctorIds,
         appointmentId: appointmentIds,
@@ -42,6 +43,40 @@ function CancelledAppointment() {
             setAppointmentIds={setAppointmentIds}
             setServiceIds={setServiceIds}
           />
+        </div>
+        <div className="w-full">
+          {appointments?.length !== 0 && appointments ? (
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+              {appointments?.map((appointmentDetail, i) => {
+                const {
+                  id,
+                  patientId,
+                  doctorId,
+                  serviceId,
+                  requestedDate,
+                  status,
+                  serviceType,
+                  doctor,
+                } = appointmentDetail || {};
+                return (
+                  <AppointmentCard
+                    id={id}
+                    patientId={patientId}
+                    doctorId={doctorId}
+                    serviceId={serviceId}
+                    requestedDate={requestedDate}
+                    status={status}
+                    serviceType={serviceType?.name}
+                    doctor={doctor?.first_name}
+                  />
+                );
+              })}
+            </div>
+          ) : (
+            <div className="flex items-center justify-center w-full">
+              <Empty />
+            </div>
+          )}
         </div>
 
         <div className="custom-table-ui">
