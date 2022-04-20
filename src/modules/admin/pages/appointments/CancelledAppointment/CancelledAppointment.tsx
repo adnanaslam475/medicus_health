f6daa0d4221d277c65cc82b31fb8b4f200c18721
Faizan Ali <faizan.ali@cooperativecomputing.com>
2022-04-20 17:32:51 +0500

    Patient responsive fixes and ui bug fixes

--- src/modules/admin/pages/appointments/CancelledAppointment/CancelledAppointment.tsx
+++ src/modules/admin/pages/appointments/CancelledAppointment/CancelledAppointment.tsx
@@ -34,16 +34,18 @@ function CancelledAppointment() {
   return (
     <AppLayout>
       <div className="w-full">
-        <div className="flex items-center justify-between">
-          <h2 className="mb-4">Cancelled Appointments</h2>
+        <div className="flex-none sm:flex items-center justify-between mb-5">
+          <div className="pr-3 mb-3 sm:mb-0">
+            <h2 className="mb-0">Cancelled Appointments</h2>
+            <h5 className="text-gray">
+            Suspendisse ac nulla non ante viverra feugiat. Duis ullamcorperequesty
+            tortor a fringilla tempus.
+            </h5>
+          </div>
           <Button type="primary" size="large">
             Request an Appointment
           </Button>
         </div>
-        <h5 className="text-gray">
-          Suspendisse ac nulla non ante viverra feugiat. Duis ullamcorperequesty
-          tortor a fringilla tempus.
-        </h5>
 
         <div className="w-5/6">
           <SearchFilters
--- src/modules/admin/pages/appointments/HistoryAppointments/HistoryAppointments.tsx
+++ src/modules/admin/pages/appointments/HistoryAppointments/HistoryAppointments.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import AppLayout from "../../../../../common/components/AppLayout/AppLayout";
 import { CloseOutlined } from "@ant-design/icons";
 import TransactionHistory from "../../../../../common/components/AccountTabs/TransactionHistory/TransactionHistory";
+import SearchFilters from "../../../../../common/components/SearchFilters/SearchFilters";
 
 const { RangePicker } = DatePicker;
 
@@ -11,47 +12,12 @@ function CancelledAppointment() {
     <AppLayout>
       <div className="w-full">
         <h2 className="mb-4">History</h2>
-        <div className="w-3/5 mb-10">
-          <div className="flex items-center">
-            <span className="mx-3">Filter</span>
-            <div className="mx-3">
-              <Select
-                placeholder="Doctor"
-                className=" lg:w-44 font-medium text-primary placeholder-primary  text-center"
-              >
-                <Select.Option
-                  className="text-primary placeholder-gray-500"
-                  value="Doctor Francis"
-                >
-                  Doctor Francis
-                </Select.Option>
-              </Select>
-            </div>
-
-            <Select
-              placeholder="Service"
-              className="mx-3 lg:w-44 font-medium text-primary placeholder-primary  text-center"
-            >
-              <Select.Option
-                className="text-primary placeholder-gray-500"
-                value="Doctor Francis"
-              >
-                Doctor Francis
-              </Select.Option>
-            </Select>
-            <Space direction="vertical" size={12} className="mx-3">
-              <RangePicker />
-            </Space>
-
-            <Button type="text" size="large" className="w-50">
-              <CloseOutlined />
-              <span className="text-gray-2 mx-3">Clear</span>
-            </Button>
-          </div>
+        <div className="w-5/6">
+          <SearchFilters />
         </div>
-        {/* Transaction History table */}
+        
         <div className="custom-table-ui">
-        <TransactionHistory />
+          <TransactionHistory />
         </div>
       </div>
     </AppLayout>
