169c12135c09a70a9327003321f62abca91e7a41
Majid <majid.mateen@cooperativecomputing.com>
2022-04-23 22:31:17 +0500

    resolved PR  issue

--- src/common/components/AccountTabs/TransactionHistory/TransactionHistory.tsx
+++ src/common/components/AccountTabs/TransactionHistory/TransactionHistory.tsx
@@ -4,137 +4,6 @@ import { EyeFilled } from "@ant-design/icons";
 import { GetAllRequestedAppointmentsQuery } from "../../../../generated/graphql";
 import { date } from "../../../utils";
 
-const Demodata = [
-  {
-    key: "1",
-    // name: "John Brown",
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    // status: ['completed', 'pending'],
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    key: "2",
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    key: "3",
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    key: "4",
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    key: "5",
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-
-  {
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    tYpe: "First Consultation",
-    booked_on: "Jan 30, 2022",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-  {
-    transactionid: "MD-2312",
-    doctor: "Dr. Paul Wallner",
-    booked_on: "Jan 30, 2022",
-    type: "First Consultation",
-    timeslot: "09:00 AM - 09:30 AM",
-    date: "Jan 30, 2022",
-    totalamount: "$40.00",
-    transactiondate: "Jan 24, 2022",
-    status: ["completed", "pending"],
-    view: "Eye",
-  },
-];
-
 interface col {
   title: string;
   dataIndex: string;
@@ -148,7 +17,7 @@ type Props = {
 
 const TransactionHistory = (props: Props) => {
   const { appoinmentHistory } = props || {};
-  console.log("appoinmentHistory", appoinmentHistory?.appointments);
+
   const columns = [
     {
       title: "ID",
