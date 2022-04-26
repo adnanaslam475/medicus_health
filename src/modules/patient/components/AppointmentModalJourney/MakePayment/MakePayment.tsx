c6287a1200562f2c709e2ba3c9ce6115ebe71dbe
adnanali-cc <adnan.ali@cooperativecomputing.com>
2022-04-26 23:39:17 +0500

    list cards for payment get card  element value

--- src/modules/patient/components/AppointmentModalJourney/MakePayment/MakePayment.tsx
+++ src/modules/patient/components/AppointmentModalJourney/MakePayment/MakePayment.tsx
@@ -4,16 +4,32 @@ import Payment from "../Payment/Payment";
 import visa from "./../../../../../../public/assets/images/visa.svg";
 import mastercard from "./../../../../../../public/assets/images/mastercard.svg";
 import _Classes from "./MakePayment.module.scss";
+import { useGetAllCardsQuery } from "../../../../../generated/graphql";
+import { getUserData } from "common/utils/userData";
+
+const CARD_TYPE = {
+  ["visa" as string]: visa,
+  ["MasterCard" as string]: mastercard,
+};
 
 function MakePayment() {
+  // GET ALL CARDS API CALL
+  const [{ data: allCardsData }] = useGetAllCardsQuery({
+    variables: { userId: getUserData()?.user?.id as number },
+  });
+
   return (
     <>
       <h2>Make Payment</h2>
       <Form layout="vertical">
         <div className="mt-8">
-          <Payment visa={visa} />
-
-          <Payment visa={mastercard} />
+          {allCardsData?.getAllCards.map((card) => (
+            <Payment
+              visa={CARD_TYPE[card?.card_type]}
+              title={`${card?.card_type} Ending with ${card?.card_digits}`}
+              description={`Expires at: ${card?.exp_month}/${card?.exp_year}`}
+            />
+          ))}
         </div>
       </Form>
     </>
--- src/modules/patient/components/AppointmentModalJourney/MakePaymentMore/MakePaymentMore.tsx
+++ src/modules/patient/components/AppointmentModalJourney/MakePaymentMore/MakePaymentMore.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import MakePaymentMorePage from './MakePaymentMorePage'
+import React from "react";
+import MakePaymentMorePage from "./MakePaymentMorePage";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import config from "../../../../../../config";
 function index() {
-    const stripePromise = loadStripe(config.stripeKey || "");
+  const stripePromise = loadStripe(config.stripeKey || "");
   return (
     <Elements stripe={loadStripe(config.stripeKey || "")}>
-    <MakePaymentMorePage/>
+      <MakePaymentMorePage />
     </Elements>
-  )
+  );
 }
 
-export default index
+export default index;
