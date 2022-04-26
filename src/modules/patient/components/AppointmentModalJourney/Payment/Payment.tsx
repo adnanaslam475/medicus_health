6c128baec9c9e0279ae08c9bfa1003543283c1dc
ali <ali.imtiaz@cooperativecomputing.com>
2022-04-26 14:19:55 +0500

    pr requested changes

--- src/modules/patient/components/AppointmentModalJourney/Payment/Payment.tsx
+++ src/modules/patient/components/AppointmentModalJourney/Payment/Payment.tsx
@@ -22,8 +22,6 @@ function Payment(props: Props) {
         />
       </div>
       <div className="px-2">
-        {/* <h6 className="pb-0 mb-0">Visa Ending with 2256</h6>
-        <p className="mb-0 text-sm">Expires: 05/2026</p> */}
         <h6 className="text-md capitalize text-dark font-medium">{title}</h6>
         <p className="text-gray-2">{description}</p>
       </div>
