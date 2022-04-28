bac40f10820a85590debdcdd6dee7aaa0be3695c
usama khan <usama.khan@cooperativecomputing.com>
2022-04-28 13:26:10 +0500

    set ui

---
+++ pages/doctor/appointments/detail.tsx
@@ -0,0 +1,10 @@
+import AppointmentsDetail from 'modules/doctor/pages/physicians/AppointmentsDetail/AppointmentsDetail'
+import React from 'react'
+
+function AppointmentDetail() {
+  return (
+    <AppointmentsDetail/>
+  )
+}
+
+export default AppointmentDetail
