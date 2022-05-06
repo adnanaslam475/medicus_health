d08802944a81b6131637f2ad57d635e3ece05708
MrFarhan <muhammadfarhan012345@gmail.com>
2022-05-06 15:35:33 +0500

    updated userPreferences query type

--- src/modules/patient/pages/EmailNotification/EmailNotification.tsx
+++ src/modules/patient/pages/EmailNotification/EmailNotification.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import EmailNotification from "../../../common/components/EmailNotification/EmailNotification";
 import ThinLine from "../../../../common/components/ThinLine/ThinLine";
-import { useUserEmailPreferencesQuery } from "generated/graphql";
+import {
+  UserEmailPreferencesResponse,
+  useUserEmailPreferencesQuery,
+} from "generated/graphql";
 import { patientEmailPreferencesData } from "utils/helper";
 
-interface userPreferencesType {
-  admin_appointment_create_update: Boolean;
-  appointment_accepted_by_doctor: Boolean;
-  appointment_reminder: Boolean;
-  appointment_rescheduled_by_doctor: Boolean;
-  new_message_received: Boolean;
-  patient_registration_update: Boolean;
-  physician_registration_update: Boolean;
-  __typename?: string;
-}
 function EmailNotificationPage() {
-  const [userEmailPreferences, executeUserEmailPreferencesQuery] =
+  const [{ data }, executeUserEmailPreferencesQuery] =
     useUserEmailPreferencesQuery();
-
-  const userPreferences: userPreferencesType | undefined =
-    userEmailPreferences?.data?.userEmailPreferences;
+  const { userEmailPreferences } = data || {};
 
   // below function will be used for mutation call
   async function ChangeHandler(value: string, valStatus: boolean) {}
@@ -27,7 +18,7 @@ function EmailNotificationPage() {
     <div>
       <div className="flex md:flex-row gap-0 max-w-[60%]">
         <div className=" w-full border py-0 rounded-lg border-gray-7">
-          {userPreferences &&
+          {userEmailPreferences &&
             patientEmailPreferencesData?.map((item) => {
               return (
                 <>
@@ -36,7 +27,9 @@ function EmailNotificationPage() {
                     key={item.key}
                     onChange={(e: boolean) => ChangeHandler(item.key, e)}
                     checked={
-                      !!userPreferences[item?.key as keyof userPreferencesType]
+                      !!userEmailPreferences[
+                        item?.key as keyof UserEmailPreferencesResponse
+                      ]
                     }
                   />
                   <ThinLine />
