0d955c11f508af0c93770efce0b2eb152121212b
usama khan <usama.khan@cooperativecomputing.com>
2022-05-16 19:01:10 +0500

    pr changes

--- src/modules/patient/pages/PatientAppointmentHistoryDetail/PatientAppointmentHistoryDetail.tsx
+++ src/modules/patient/pages/PatientAppointmentHistoryDetail/PatientAppointmentHistoryDetail.tsx
@@ -1,56 +1,20 @@
 import React from "react";
-import AppointmentTabs from "common/components/Appointments/AppointmentTabs";
 import AppLayout from "common/components/AppLayout/AppLayout";
 import { useRouter } from "next/router";
 import {
 	useDoctorAppointmentDetailQuery,
 	useGetAppointmentReportUrlByIdQuery,
-	usePatientHealthHistoryQuery,
+	
 } from "generated/graphql";
 import { Tabs } from "antd";
-import NotesWithText from "common/components/NotesWithText/NotesWithText";
 import PatientAppointmentInfoTab from "./PatientAppointmentInfoTab";
 import PatientInfoTab from "./PatientInfoTab";
-import PhysicianQuestionnaire from "common/components/Appointments/PhysicianQuestionnaire";
 import HealthQuestionnaireFrom from "./HealthQuestionnaireFromTab";
 import PhysicianQuestionnaireForm from "./PhysicianQuestionnaireFormTab";
 import AttachmentTab from "./AttachmentTab";
+import NoteWithTextTab from "./NoteWithTextTab";
 
 function PatientAppointmentHistoryDetail() {
-	const { query } = useRouter();
-
-	const [{ data }] = useDoctorAppointmentDetailQuery({
-		variables: {
-			id: Number(query.appointmentId),
-		},
-		pause: !query.appointmentId,
-	});
-	const { appointment } = data || {};
-
-	const [{ data: appoinmentUrl }] = useGetAppointmentReportUrlByIdQuery({
-		variables: {
-			id: Number(appointment?.id),
-		},
-	});
-
-	const { reportUrl } = appoinmentUrl?.appointment || {};
-
-	function parseJson(jsonString: string) {
-		let obj = null;
-		try {
-			obj = JSON.parse(jsonString);
-		} catch (error) {
-			console.log(error);
-			obj = null;
-		}
-		return obj;
-	}
-
-	let urlArr = parseJson(reportUrl);
-	if (urlArr && urlArr.length > 0) {
-		urlArr = urlArr[0]?.map((item: any) => item.split("com/")[1]);
-	}
-
 	return (
 		<AppLayout>
 			<>
@@ -73,9 +37,7 @@ function PatientAppointmentHistoryDetail() {
 							<AttachmentTab/>
 						</Tabs.TabPane>
 						<Tabs.TabPane tab="Notes" key="6">
-							<div className="max-w-1/2">
-								<NotesWithText />
-							</div>
+							<NoteWithTextTab/>
 						</Tabs.TabPane>
 					</Tabs>
 				</div>
