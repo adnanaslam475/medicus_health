3eb16f9614a54b241c8a4157687537bc6a5bbecd
Yasir Hussain <yasir9001@yahoo.com>
2022-05-16 14:51:34 +0500

    added types for video call component

--- src/modules/patient/pages/appointment/VideoCall/Video.tsx
+++ src/modules/patient/pages/appointment/VideoCall/Video.tsx
@@ -1,11 +1,13 @@
-//@ts-nocheck
-import { AgoraVideoPlayer } from "agora-rtc-react";
+import { AgoraVideoPlayer, IMicrophoneAudioTrack ,ICameraVideoTrack,IAgoraRTCRemoteUser} from "agora-rtc-react";
 import React from "react";
 
 // scss
 import _classes from "./VideoCall.module.scss";
 
-type Props = {};
+type Props = {
+  tracks: [IMicrophoneAudioTrack, ICameraVideoTrack];
+  users: IAgoraRTCRemoteUser[];
+};
 
 function Video(props: Props) {
   const { users, tracks } = props;
@@ -15,7 +17,10 @@ function Video(props: Props) {
   return (
     <>
       <div className=" absolute right-4 top-4 z-10">
-        <AgoraVideoPlayer videoTrack={tracks[1]} className="flex-1 h-[150px] w-[200px]" />
+        <AgoraVideoPlayer
+          videoTrack={tracks[1]}
+          className="flex-1 h-[150px] w-[200px]"
+        />
       </div>
       {isShowParticipant && (
         <>
