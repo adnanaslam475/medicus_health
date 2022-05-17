5f5c86a3377f8912675ca539f1c6f35186a9130e
MrFarhan <muhammadfarhan012345@gmail.com>
2022-05-17 17:40:47 +0500

    implemented appointment history with filter

--- src/common/types/types.tsx
+++ src/common/types/types.tsx
@@ -1,3 +1,5 @@
+import { BookingDate, DueDate, InputMaybe, Scalars } from "generated/graphql";
+
 export type Schedule = {
   day?: number;
   endTime: string;
@@ -19,4 +21,22 @@ export type physicianFilterType = {
     startDate?: string;
     endDate?: string;
   };
+};
+
+export type PhysicianAppointmentInputFilter = {
+  appointmentId?: InputMaybe<Scalars["Int"]>;
+  bookingDate?: InputMaybe<BookingDate>;
+  doctorId?: InputMaybe<Scalars["Int"]>;
+  dueDate?: InputMaybe<DueDate>;
+  patientId?: InputMaybe<Scalars["Int"]>;
+  physicianName?: InputMaybe<Scalars["String"]>;
+  searchPatient?: InputMaybe<Scalars["String"]>;
+  serviceId?: InputMaybe<Scalars["Int"]>;
+  status?: InputMaybe<Scalars["String"]>;
+  paymentStatus?: string;
+};
+
+export type DateType = {
+  startDate: string;
+  endDate: string;
 };
--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -418,6 +418,7 @@ export type GetTransectionInput = {
   appointmentId?: InputMaybe<Scalars['Int']>;
   earnings?: InputMaybe<EarningRange>;
   searchString?: InputMaybe<Scalars['String']>;
+  serviceId?: InputMaybe<Scalars['Int']>;
   serviceName?: InputMaybe<Scalars['String']>;
   transectionId?: InputMaybe<Scalars['Int']>;
 };
@@ -1406,6 +1407,13 @@ export type PhysiciansPatientsQueryVariables = Exact<{
 
 export type PhysiciansPatientsQuery = { __typename?: 'Query', physiciansPatients: Array<{ __typename?: 'User', id: number, first_name: string, last_name: string, email: string, contact_number?: string | null, streetAddress?: string | null, patientProfile?: { __typename?: 'PatientProfile', profileImage?: string | null } | null }> };
 
+export type PhysicianAppointmentsHistoryQueryVariables = Exact<{
+  filter: GetAppointmentInput;
+}>;
+
+
+export type PhysicianAppointmentsHistoryQuery = { __typename?: 'Query', appointments: Array<{ __typename?: 'Appointment', id: number, createdAt: any, requestedDate: any, status?: string | null, serviceType?: { __typename?: 'AppointmentServiceType', name: string } | null, patient: { __typename?: 'User', first_name: string, last_name: string }, appointmentTimeSlots?: Array<{ __typename?: 'AppointmentTimeSlots', startTime: any, endTime: any, selected: boolean }> | null, transaction?: { __typename?: 'Transaction', status: string, amountReceived: number } | null }> };
+
 export type CountriesQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -2144,6 +2152,36 @@ export const PhysiciansPatientsDocument = gql`
 
 export function usePhysiciansPatientsQuery(options?: Omit<Urql.UseQueryArgs<PhysiciansPatientsQueryVariables>, 'query'>) {
   return Urql.useQuery<PhysiciansPatientsQuery>({ query: PhysiciansPatientsDocument, ...options });
+};
+export const PhysicianAppointmentsHistoryDocument = gql`
+    query physicianAppointmentsHistory($filter: GetAppointmentInput!) {
+  appointments(filter: $filter) {
+    id
+    createdAt
+    requestedDate
+    serviceType {
+      name
+    }
+    patient {
+      first_name
+      last_name
+    }
+    appointmentTimeSlots {
+      startTime
+      endTime
+      selected
+    }
+    status
+    transaction {
+      status
+      amountReceived
+    }
+  }
+}
+    `;
+
+export function usePhysicianAppointmentsHistoryQuery(options: Omit<Urql.UseQueryArgs<PhysicianAppointmentsHistoryQueryVariables>, 'query'>) {
+  return Urql.useQuery<PhysicianAppointmentsHistoryQuery>({ query: PhysicianAppointmentsHistoryDocument, ...options });
 };
 export const CountriesDocument = gql`
     query countries {
