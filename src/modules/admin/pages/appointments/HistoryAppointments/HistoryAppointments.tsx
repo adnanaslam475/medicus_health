5b315a7116682c80013ed97f90b15a26f21d96ce
MrFarhan <muhammadfarhan012345@gmail.com>
2022-05-20 20:13:59 +0500

    Added input on patient history tab and enhanced datepicker

--- src/modules/admin/pages/appointments/HistoryAppointments/HistoryAppointments.tsx
+++ src/modules/admin/pages/appointments/HistoryAppointments/HistoryAppointments.tsx
@@ -1,9 +1,17 @@
-import { Select, DatePicker, Space, Button } from "antd";
-import React from "react";
+import { Select, DatePicker, Space, Button, Input } from "antd";
+import React, { useState } from "react";
 import AppLayout from "common/components/AppLayout/AppLayout";
-import { CloseOutlined } from "@ant-design/icons";
-import { Appointment, useGetAllRequestedAppointmentsQuery } from "generated/graphql";
+import {
+  CloseOutlined,
+  SearchOutlined,
+  CaretDownOutlined,
+} from "@ant-design/icons";
+import {
+  Appointment,
+  useGetAllRequestedAppointmentsQuery,
+} from "generated/graphql";
 import AppointmentHistoryTable from "common/components/AppointmentHistoryTable/AppointmentHistoryTable";
+import _classes from "./HistoryAppointments.module.scss";
 
 const { RangePicker } = DatePicker;
 
@@ -16,9 +24,14 @@ function CancelledAppointment() {
       },
     },
   });
+  const [openDateRange1, setOpenDateRange1] = useState(false);
 
   const { appointments } = data || {};
 
+  const physicians = ["Dr Abc", "Dr Def", "Dr Ghi"];
+  const applyDateRange = () => {
+    setOpenDateRange1(false);
+  };
   return (
     <AppLayout>
       <div className="w-full">
@@ -33,10 +46,30 @@ function CancelledAppointment() {
         <div className="w-5/6 mb-10">
           <div className="flex items-center">
             <span className="mx-3">Filter</span>
+            <div className="lg:ml-3 w-full sm:w-full md:w-full lg:w-70 mr-2">
+              <Input
+                placeholder="Search by ID or physician name"
+                prefix={<SearchOutlined />}
+              />
+            </div>
+
             <div className="mx-3">
               <Select
                 placeholder="Doctor"
                 className=" lg:w-44 font-medium text-primary placeholder-primary  text-center"
+              >
+                {physicians?.map((item) => (
+                  <Select.Option key={item} value={item}>
+                    {item}
+                  </Select.Option>
+                ))}
+              </Select>
+            </div>
+
+            <div className="w-full md:w-44 xl:w-60 mr-3">
+              <Select
+                placeholder="Service"
+                className="mx-3 lg:w-44 font-medium text-primary placeholder-primary  text-center"
               >
                 <Select.Option
                   className="text-primary placeholder-gray-500"
@@ -47,24 +80,63 @@ function CancelledAppointment() {
               </Select>
             </div>
 
-            <Select
-              placeholder="Service"
-              className="mx-3 lg:w-44 font-medium text-primary placeholder-primary  text-center"
-            >
-              <Select.Option
-                className="text-primary placeholder-gray-500"
-                value="Doctor Francis"
+            <div className="flex-none sm:flex -mt-3">
+              <Space
+                direction="vertical"
+                size={0}
+                className="w-full md:w-44 xl:w-60 sm:mb-3"
               >
-                Doctor Francis
-              </Select.Option>
-            </Select>
-            <Space direction="vertical" size={12} className="mx-3">
-              <RangePicker />
-            </Space>
+                <div className="relative">
+                  <RangePicker
+                    value={null}
+                    open={openDateRange1}
+                    className="h-0 overflow-hidden text-black p-0 absolute bottom-0 invisible"
+                    renderExtraFooter={() => (
+                      <div className="flex gap-3 justify-end p-3">
+                        <Button
+                          className="bg-gray-300"
+                          onClick={() => {
+                            setOpenDateRange1(false);
+                          }}
+                        >
+                          Cancel
+                        </Button>
+                        <Button
+                          className=" text-white"
+                          type="primary"
+                          onClick={() => {
+                            applyDateRange();
+                          }}
+                        >
+                          Apply
+                        </Button>
+                      </div>
+                    )}
+                  />
+                  <Button
+                    className="flex date-btn"
+                    block
+                    type="default"
+                    onClick={() => setOpenDateRange1?.(!openDateRange1)}
+                  >
+                    <div className="flex justify-between items-center w-full px-3">
+                      <div>Date</div>
+                      <div>
+                        <CaretDownOutlined style={{ color: `primary` }} />
+                      </div>
+                    </div>
+                  </Button>
+                </div>
+              </Space>
+            </div>
 
-            <Button type="text" size="large" className="w-50">
+            <Button
+              type="text"
+              size="middle"
+              className={`${_classes["historyClearBtn"]} w-50 ml-0 pl-0`}
+            >
               <CloseOutlined />
-              <span className="text-gray-2 mx-3">Clear</span>
+              <span className="text-gray-2 mx-1">Clear</span>
             </Button>
           </div>
         </div>
