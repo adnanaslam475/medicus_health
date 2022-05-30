0ad94848631d347fe96987ec029c602922660f00
ali <ali.imtiaz@cooperativecomputing.com>
2022-05-30 21:56:06 +0500

    MDCS-1296 Update Health Questionare by Admin

--- src/modules/admin/pages/AdminPatientListingDetail/AdminHealthQuestionnaireFromTab.tsx
+++ src/modules/admin/pages/AdminPatientListingDetail/AdminHealthQuestionnaireFromTab.tsx
@@ -1,8 +1,14 @@
 import CardWithProfileImageInfo from "common/components/CardWithProfileImageInfo/CardWithProfileImageInfo";
 import { QuestionnaireForm } from "common/components/Questionnary/Questionnary";
 import { useRouter } from "next/router";
-import { usePhysicianAppointmentsHistoryQuery } from "generated/graphql";
-import React from "react";
+import {
+  usePatientHealthHistoryQuery,
+  usePhysicianAppointmentsHistoryQuery,
+  useUpdatePatientHealthHistoryMutation,
+} from "generated/graphql";
+import React, { useRef } from "react";
+import { Button, notification } from "antd";
+import { getUserData } from "common/utils/userData";
 
 function AdminHealthQuestionnaireFrom() {
   const { query } = useRouter();
@@ -14,6 +20,45 @@ function AdminHealthQuestionnaireFrom() {
   });
   const { appointments } = data || {};
   const appointment = appointments && appointments[0];
+
+  const form: any = useRef();
+
+  // GET USER ID
+  const { user } = getUserData();
+  const id = user?.id;
+
+  // Get patient Health History
+  const [{ data: patientHealthHistoryData }] = usePatientHealthHistoryQuery({
+    variables: { input: id as number },
+  });
+
+  // UPDATE PATIENT HEALTH HISTORY
+
+  const [result, updatePatientHealthHistory] =
+    useUpdatePatientHealthHistoryMutation();
+
+  const { error, fetching } = result;
+
+  const onFinishHealthQuestionnarySuccess = async (quesPayload: any) => {
+    const healthQuesJson = JSON.stringify(quesPayload);
+    try {
+      const res = await updatePatientHealthHistory({
+        input: {
+          history: healthQuesJson,
+          user_id: id as number,
+        },
+      });
+      {
+        res?.data?.updatePatientHealthHistory &&
+          notification.success({
+            message: "Successfully Updated",
+          });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="max-w-1/2">
       <CardWithProfileImageInfo
@@ -22,7 +67,21 @@ function AdminHealthQuestionnaireFrom() {
       >
         <QuestionnaireForm
           data={appointment?.patient?.patientHealthHistory?.history}
+          ref={form}
+          // data={data?.patientHealthHistory.history}
+          onFinishSuccess={onFinishHealthQuestionnarySuccess}
         />
+        <div className="flex items-center justify-end">
+          <Button
+            loading={fetching}
+            disabled={fetching}
+            className="ant-btn ant-btn-primary ant-btn mb-0"
+            type="primary"
+            onClick={() => form?.current?.submit()}
+          >
+            Update
+          </Button>
+        </div>
       </CardWithProfileImageInfo>
     </div>
   );
