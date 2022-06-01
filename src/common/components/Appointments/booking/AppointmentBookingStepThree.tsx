0cc63f0dde30430a7e27fee48222a4a74c2bafa8
MrFarhan <muhammadfarhan012345@gmail.com>
2022-06-01 16:50:25 +0500

    Added last filled form functionality on physician book appointment button

--- src/common/components/Appointments/booking/AppointmentBookingStepThree.tsx
+++ src/common/components/Appointments/booking/AppointmentBookingStepThree.tsx
@@ -1,24 +1,16 @@
 import React, { useEffect } from "react";
-import {
-  Divider,
-  Radio,
-  Checkbox,
-  Form,
-  Input,
-  Button,
-  Select,
-  DatePicker,
-} from "antd";
+import { Radio, Checkbox, Form, Input } from "antd";
 import { useBookAppointment } from "../../BookAppointmentJourney/BookAppointmentContext";
 import {
   DoctorProfile,
-  useDoctorProfileQuery,
   useDoctorQuestionnaireQuery,
-  useGetAllRequestedAppointmentsQuery,
-} from "../../../../generated/graphql";
+  usePatientLastQuestionnaireQuery,
+} from "generated/graphql";
 import { useRouter } from "next/router";
 import { NamePath } from "antd/lib/form/interface";
 import { parseJson } from "common/utils/helper";
+import { CheckboxChangeEvent } from "antd/lib/checkbox";
+import { getUserData } from "common/utils/userData";
 
 type Props = {
   physicianData?: DoctorProfile;
@@ -30,52 +22,78 @@ const StepThree = React.forwardRef(function StepThree(props: Props, ref: any) {
   const { id } = physicianData?.user || {};
   const { saveStepThree, data } = useBookAppointment();
   const [formInstance] = Form.useForm();
-  const physicianId = data?.stepOne?.physician?.split(":")[0]
+  const physicianId = data?.stepOne?.physician?.split(":")[0];
   const [{ data: dataList }] = useDoctorQuestionnaireQuery({
     variables: {
       doctorId: Number(physicianId) || Number(query?.id) || Number(id),
     },
   });
   const { doctorQuestionnaire } = dataList || {};
+  const { user } = getUserData();
+  const loggedinPatientId = user?.id;
 
-
+  const [{ data: patientLastQuestionaryData }] =
+    usePatientLastQuestionnaireQuery({
+      variables: {
+        patientId: Number(loggedinPatientId),
+        doctorId: Number(id),
+      },
+    });
+  const { patientLastQuestionnaire } = patientLastQuestionaryData || {};
   function onFinishLocal(values: any) {
-    saveStepThree?.(values);
+    saveStepThree?.({ ...values, isLastFilled: data?.stepThree?.isLastFilled });
   }
-
   useEffect(() => {
     if (ref) {
       ref.current = formInstance;
     }
-    formInstance.setFieldsValue({
-      ...data.stepThree,
-    });
-    prepareAndSetEditPayload();
   }, []);
+
+  useEffect(() => {
+    prepareAndSetEditPayload();
+  }, [data.stepThree]);
 
   function prepareAndSetEditPayload() {
     formInstance.setFieldsValue({
       doctorId: doctorQuestionnaire?.doctorId,
       id: doctorQuestionnaire?.id,
-      // questionnaire: doctorQuestionnaire?.questionnaire,
+      ...data.stepThree,
     });
   }
 
   let questionnair = parseJson(doctorQuestionnaire?.questionnaire);
 
+  const checkBoxHandler = (e: CheckboxChangeEvent) => {
+    let formatedQuestioner = parseJson(patientLastQuestionnaire?.history);
+    if (e?.target?.checked) {
+      saveStepThree?.({
+        ...formatedQuestioner,
+        isLastFilled: e?.target?.checked,
+      });
+    } else {
+      saveStepThree?.(undefined);
+      formInstance.resetFields();
+    }
+  };
   return (
     <>
       <h2>Request an Appointment</h2>
       <Form layout="vertical" form={formInstance} onFinish={onFinishLocal}>
-        {/* <Form.Item>
-          <div className="w-full bg-gray-4 border border-gray-3 rounded flex items-center p-3">
-            <Checkbox value="0">
-              <span className="text-gray-2">
-                I want to use my last filled form
-              </span>
-            </Checkbox>
-          </div>
-        </Form.Item> */}
+        {doctorQuestionnaire && (
+          <Form.Item valuePropName="checked">
+            <div className="w-full bg-gray-4 border border-gray-3 rounded flex items-center p-3">
+              <Checkbox
+                value={data?.stepThree?.isLastFilled || 0}
+                onChange={(e) => checkBoxHandler(e)}
+                checked={data?.stepThree?.isLastFilled || false}
+              >
+                <span className="text-gray-2">
+                  I want to use my last filled form
+                </span>
+              </Checkbox>
+            </div>
+          </Form.Item>
+        )}
         {questionnair?.map(
           (
             item: {
@@ -92,13 +110,6 @@ const StepThree = React.forwardRef(function StepThree(props: Props, ref: any) {
                   label={item.label}
                   className="text-secondary"
                   name={item.name}
-                  // rules={[
-                  //   {
-                  //     required: true,
-                  //     message: "Please fill the blank",
-                  //     whitespace: true,
-                  //   },
-                  // ]}
                 >
                   <Input />
                 </Form.Item>
@@ -109,13 +120,6 @@ const StepThree = React.forwardRef(function StepThree(props: Props, ref: any) {
                   label={item.label}
                   className="text-secondary"
                   name={item.name}
-                  // rules={[
-                  //   {
-                  //     required: true,
-                  //     message: "Please select the option",
-                  //     whitespace: true,
-                  //   },
-                  // ]}
                 >
                   <Radio.Group>
                     {item?.options?.map(({ value, label }) => {
@@ -127,13 +131,6 @@ const StepThree = React.forwardRef(function StepThree(props: Props, ref: any) {
             }
           }
         )}
-        {/* <Form.Item>
-          <div className="flex items-center justify-end">
-            <Button type="primary" htmlType="submit">
-              Save
-            </Button>
-          </div>
-        </Form.Item> */}
       </Form>
     </>
   );
