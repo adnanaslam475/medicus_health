88434a108a6e0ed09dae7fbb86c2aa30ed2fc2fa
usama khan <usama.khan@cooperativecomputing.com>
2022-06-03 15:02:22 +0500

    set dashbard

--- src/common/components/StatisticsCard/StatisticsCard.tsx
+++ src/common/components/StatisticsCard/StatisticsCard.tsx
@@ -10,13 +10,13 @@ type Props = {
 function StatisticsCard({ src, title, value }: Props) {
 	return (
 		<div
-			className={`bg-gray-4 border border-gray-4 flex gap-2 flex-col shadow-md py-20 px-4 mx-4 my-3`}
+			className={`bg-gray-4 border border-gray-4 flex flex-col shadow-md py-20 px-4 mx-4 my-3`}
 		>
 			<div className="rounded-full bg-white px-3 border border-gray-4 mx-auto p-2 shadow-md text-center">
 				<Image alt="" className="" height={28} width={28} src={src} />
 			</div>
 
-			<div className="text-6xl mx-auto px-auto text-center">{value}</div>
+			<div className="text-6xl mx-auto px-auto text-center py-2">{value}</div>
 			<div className="text-center text-xl">{title} </div>
 		</div>
 	);
