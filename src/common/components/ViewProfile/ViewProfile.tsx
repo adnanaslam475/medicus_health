3636e26c5020dc8905f4134befc816319a8dd0f8
Majid <majid.mateen@cooperativecomputing.com>
2022-06-09 14:43:00 +0500

    fixed MDCS-1185

--- src/common/components/ViewProfile/ViewProfile.tsx
+++ src/common/components/ViewProfile/ViewProfile.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { EditOutlined } from "@ant-design/icons";
 import { Avatar, Form, Button } from "antd";
 
-import { User,DoctorProfile } from "generated/graphql";
+import { User, DoctorProfile } from "generated/graphql";
 import { Schedule } from "common/types/types";
 import { parseJson } from "common/utils/helper";
 import ProfileForm from "./ProfileForm";
@@ -96,13 +96,6 @@ export const ViewProfile = React.forwardRef(function Profile({
               </h2>
               <span className="block">{email}</span>
               <div className="flex gap-2 pt-2">
-                <Button
-                  type="primary"
-                  className={`${_classes["published-button"]}`}
-                >
-                  {status ? "Published" : "Unpublished"}
-                </Button>
-
                 <Button
                   type="default"
                   className={`${_classes["edit-button"]}`}
