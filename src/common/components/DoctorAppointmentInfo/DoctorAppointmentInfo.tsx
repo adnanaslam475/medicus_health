92ff913623c20e85d51b3f33bbedc914db0b7fc3
Majid <majid.mateen@cooperativecomputing.com>
2022-06-09 15:57:48 +0500

    fixed chat issue in physician requested appointments

--- src/common/components/DoctorAppointmentInfo/DoctorAppointmentInfo.tsx
+++ src/common/components/DoctorAppointmentInfo/DoctorAppointmentInfo.tsx
@@ -80,8 +80,6 @@ function DoctorAppointmentInfo({ data }: Props) {
   const { user } = getUserData();
   const { id: doctorId } = user || {};
 
-  console.log(data, "sadasdsadsad===>");
-
   const [{ fetching: cancelFetching }, executeCancelRequestedAppointment] =
     useCancelAppointmentByDoctorMutation();
 
@@ -215,7 +213,13 @@ function DoctorAppointmentInfoFooter({
   appointmentId: number | undefined;
   data?: Appointment;
 }) {
+  // GET USER ID
+  const { user } = getUserData();
+  const doctorId = user?.id;
   const { appointmentTimeSlots } = data || {};
+
+  const { patient } = data || {};
+  const { id: patientId } = patient || {};
   const selectedAppointment: AppointmentTimeSlots | undefined = useMemo(
     () => appointmentTimeSlots?.find((item) => item.selected),
     [appointmentTimeSlots]
@@ -232,16 +236,34 @@ function DoctorAppointmentInfoFooter({
         <Button
           icon={<MessageOutlined />}
           className={`${_classes["appointments-btn"]} mr-3`}
-          onClick={() => Router.push("/admin/messages")}
+          onClick={() =>
+            Router.push({
+              pathname: "/physician/messages",
+              query: {
+                chat: "admin",
+                doctorId: doctorId,
+                patientId: patientId,
+              },
+            })
+          }
         >
           Message Admin
         </Button>
         <Button
           icon={<MessageOutlined />}
           className={`${_classes["appointments-btn"]}`}
-          onClick={() => Router.push("/physician/messages")}
+          onClick={() =>
+            Router.push({
+              pathname: "/physician/messages",
+              query: {
+                chat: "patient",
+                doctorId: doctorId,
+                patientId: patientId,
+              },
+            })
+          }
         >
-          Message Physician
+          Message Patient
         </Button>
       </div>
       <Button
