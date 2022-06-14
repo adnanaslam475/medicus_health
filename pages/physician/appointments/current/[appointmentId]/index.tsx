4a6f19ce71ad85ffa13a8a9e00ac7058685776c5
usamahassankhan <usamahassankhan870@gmail.com>
2022-06-14 07:59:29 -0700

    set listig current to physician

---
+++ pages/physician/appointments/current/[appointmentId]/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CancelledAppointmentsDetailDoctor from "modules/doctor/pages/appointments/CancelledAppointmentsDetailDoctor/CancelledAppointmentsDetailDoctor";
+
+function appointmentId() {
+  return <CancelledAppointmentsDetailDoctor />;
+}
+
+export default appointmentId;
