371e920c250dfa13c4b2a65ac2133a5cf0af17ab
MrFarhan <muhammadfarhan012345@gmail.com>
2022-06-20 22:30:49 +0500

    Added filters on patient current Appointment list view

--- src/modules/admin/pages/appointments/CurrentAppointment/CurrentAppointment.tsx
+++ src/modules/admin/pages/appointments/CurrentAppointment/CurrentAppointment.tsx
@@ -2,98 +2,90 @@ import React, { useState } from "react";
 import { Empty, Spin } from "antd";
 import AppointmentCard from "common/components/AppointmentCard/AppointmentCard";
 import AppLayout from "common/components/AppLayout/AppLayout";
-import SearchFilters from "common/components/SearchFilters/SearchFilters";
+import SearchFilter from "common/components/SearchFilters/SearchFilter";
 import {
-	AppointmentDateTimeResponse,
-	AppointmentTimeSlots,
-	BookingDate,
-	useCurrentAppointmentsQuery,
+  AppointmentDateTimeResponse,
+  AppointmentTimeSlots,
+  useCurrentAppointmentsQuery,
 } from "generated/graphql";
 
 function CurrentAppointment() {
-	const [dueDates, setDueDates] = useState<Date | null>();
-	const [dueStartDate, setStartDate] = useState<Date | null>();
-	const [dueEndDate, setEndDate] = useState<Date | null>();
-	const [bookingDate, setBookingDate] = useState<BookingDate>({});
-	const [dataListPhysician, setDataListPhysician] = useState<string>();
-	const [doctorIds, setDoctorId] = useState<number>();
-	const [appointmentId, setAppointmentId] = useState<number>();
-	const [currentAppointmentId, setCurrentAppointmentId] = useState<number>();
-	const [serviceIds, setServiceIds] = useState<number>();
-	const [status, setStatus] = useState<string>("Cancelled"); //it wll be replaced by current when api will be  integrated
-	const [{ data,fetching}] = useCurrentAppointmentsQuery({
-		variables: {
-		},
-	});
+  const [filterValues, setFilterValues] = useState({});
+  const [{ data, fetching },executeUseCurrentAppointmentsQuery] = useCurrentAppointmentsQuery({
+    variables: { filter: filterValues },
+  });
 
-	function onViewSuggestedSlots(id: number) {
-		setCurrentAppointmentId(id);
-		setShowModal(true);
-	}
-	const { currentAppointments } = data || {};
-	const [showModal, setShowModal] = useState<boolean>(false);
+  const { currentAppointments } = data || {};
+  const [showModal, setShowModal] = useState<boolean>(false);
 
-	return (
-		<AppLayout>
-			<div className="w-full">
-				<div className="flex-none sm:flex items-center justify-between mb-5">
-					<div className="pr-3 mb-3 sm:mb-0">
-						<h2 className="mb-0">Current Appointments</h2>
-						<h5 className="text-gray">
-							Suspendisse ac nulla non ante viverra feugiat. Duis
-							ullamcorperequesty tortor a fringilla tempus.
-						</h5>
-					</div>
-				
-				</div>
+  function onChangeFilters(values: any) {
+    setFilterValues(values);
+    executeUseCurrentAppointmentsQuery({
+      filter: filterValues,
+      requestPolicy: "network-only",
+    });
+  }
 
-				<div className="w-5/6">
-					<SearchFilters
-						setStartDate={setStartDate}
-						setEndDate={setEndDate}
-						setDataListPhysician={setDataListPhysician}
-						setDoctorId={setDoctorId}
-						setAppointmentId={setAppointmentId}
-						setServiceIds={setServiceIds}
-						setBookingDate={setBookingDate}
-					/>
-				</div>
-				{fetching == false ? (
-					<div className="w-full">
-						{currentAppointments?.length ? (
-							<div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-								{currentAppointments?.map((currentAppointment) => {
-									return (
-										<AppointmentCard
-											doctorId={currentAppointment?.doctorId }
-											patientId={currentAppointment?.patientId}
-											requestedDate={currentAppointment?.appointmentDateTime?.startTime || ""}
-											appointmentId={Number(currentAppointment?.id)}
-											// status={status}
-											status="Current"
-											serviceType={currentAppointment?.serviceType?.name || "Service type"}
-											doctor={currentAppointment?.doctor?.first_name}
-											appointmentTimeSlots={currentAppointment?.appointmentTimeSlots as AppointmentTimeSlots[]}
-											appointmentDateTime={currentAppointment?.appointmentDateTime as AppointmentDateTimeResponse}
-											onViewSuggestedSlots={() => {}}
-											setShowModal={setShowModal}
-										/>
-									);
-								})}
-							</div>
-						) : (
-							<div className="flex items-center justify-center w-full">
-								<Empty />
-							</div>
-						)}
-					</div>
-				) : (
-					<div className="w-full flex justify-center py-10">
-						<Spin/>
-					</div>
-				)}
-			</div>
-		</AppLayout>
-	);
+  return (
+    <AppLayout>
+      <div className="w-full">
+        <div className="flex-none sm:flex items-center justify-between mb-5">
+          <div className="pr-3 mb-3 sm:mb-0">
+            <h2 className="mb-0">Current Appointments</h2>
+            <h5 className="text-gray">
+              Suspendisse ac nulla non ante viverra feugiat. Duis
+              ullamcorperequesty tortor a fringilla tempus.
+            </h5>
+          </div>
+        </div>
+
+        <div className="w-5/6">
+          <SearchFilter onChange={onChangeFilters} />
+        </div>
+        {!fetching ? (
+          <div className="w-full">
+            {currentAppointments?.length ? (
+              <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+                {currentAppointments?.map((currentAppointment) => {
+                  return (
+                    <AppointmentCard
+                      doctorId={currentAppointment?.doctorId}
+                      patientId={currentAppointment?.patientId}
+                      requestedDate={
+                        currentAppointment?.appointmentDateTime?.startTime || ""
+                      }
+                      appointmentId={Number(currentAppointment?.id)}
+                      // status={status}
+                      status="Current"
+                      serviceType={
+                        currentAppointment?.serviceType?.name || "Service type"
+                      }
+                      doctor={currentAppointment?.doctor?.first_name}
+                      appointmentTimeSlots={
+                        currentAppointment?.appointmentTimeSlots as AppointmentTimeSlots[]
+                      }
+                      appointmentDateTime={
+                        currentAppointment?.appointmentDateTime as AppointmentDateTimeResponse
+                      }
+                      onViewSuggestedSlots={() => {}}
+                      setShowModal={setShowModal}
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <div className="flex items-center justify-center w-full">
+                <Empty />
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="w-full flex justify-center py-10">
+            <Spin />
+          </div>
+        )}
+      </div>
+    </AppLayout>
+  );
 }
 export default CurrentAppointment;
