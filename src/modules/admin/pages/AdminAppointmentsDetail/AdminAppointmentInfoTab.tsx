7de20fdcc5c9c771af96b40011ecaf17c8baf670
MrFarhan <muhammadfarhan012345@gmail.com>
2022-06-21 20:51:14 +0500

    Replaced appointmentTimeSlot with appointmentDateTime

--- src/modules/admin/pages/AdminAppointmentsDetail/AdminAppointmentInfoTab.tsx
+++ src/modules/admin/pages/AdminAppointmentsDetail/AdminAppointmentInfoTab.tsx
@@ -45,12 +45,12 @@ function AdminAppointmentInfoTab({ appointment }: Props) {
     physician:
       appointment?.doctor?.first_name + " " + appointment?.doctor?.last_name,
     service: appointment?.serviceType?.name,
-    dueDate: formatMMMM_Dcoma_YYYY(selectedAppointment?.startTime),
+    dueDate: formatMMMM_Dcoma_YYYY(appointment?.appointmentDateTime?.startTime || selectedAppointment?.startTime),
     time: `${
-      selectedAppointment?.startTime
+      appointment?.appointmentDateTime?.startTime && appointment?.appointmentDateTime?.endTime
         ? `${date?.formathhmma(
-            selectedAppointment?.startTime
-          )} - ${date?.formathhmma(selectedAppointment?.endTime)}`
+            appointment?.appointmentDateTime?.startTime
+          )} - ${date?.formathhmma(appointment?.appointmentDateTime?.endTime)}`
         : "--"
     }`,
     totalAmount: appointment?.charges,
