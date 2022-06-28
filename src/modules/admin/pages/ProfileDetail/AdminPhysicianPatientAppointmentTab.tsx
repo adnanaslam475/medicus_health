158212ad7a9e391d0b3a353919263f2bb728754b
MrFarhan <muhammadfarhan012345@gmail.com>
2022-06-28 15:47:57 +0500

    Added profile physician card details in admin physician appointments

--- src/modules/admin/pages/ProfileDetail/AdminPhysicianPatientAppointmentTab.tsx
+++ src/modules/admin/pages/ProfileDetail/AdminPhysicianPatientAppointmentTab.tsx
@@ -1,12 +1,36 @@
+import { Skeleton } from "antd";
 import CardWithProfileImageInfo from "common/components/CardWithProfileImageInfo/CardWithProfileImageInfo";
+import { useDoctorProfileQuery } from "generated/graphql";
+import { useRouter } from "next/router";
 import React from "react";
 import AdminPhysicianPatientAppointment from "../AdminPhysicianPatientAppointment/AdminPhysicianPatientAppointment";
 function AdminPhysicianPatientAppointmentTab() {
-	return (
-		<CardWithProfileImageInfo name="usama" serviceName="consultation">
-			<AdminPhysicianPatientAppointment />
-		</CardWithProfileImageInfo>
-	);
+  const { query } = useRouter();
+
+  const [{ data, fetching: loading }] = useDoctorProfileQuery({
+    variables: { doctor_id: Number(query?.id) as number },
+    pause: !Number(query?.id),
+  });
+  const { doctorProfile } = data || {};
+  const userName = `${doctorProfile?.user?.first_name} ${doctorProfile?.user?.last_name}`;
+  const profilePicture = doctorProfile?.profile_image;
+  const specialization = doctorProfile?.specialization;
+
+  return (
+    <Skeleton
+      loading={loading || !doctorProfile?.user?.first_name}
+      paragraph={{ rows: 1 }}
+      active
+    >
+      <CardWithProfileImageInfo
+        name={userName}
+        serviceName={String(specialization)}
+        imageUrl={profilePicture}
+      >
+        <AdminPhysicianPatientAppointment />
+      </CardWithProfileImageInfo>
+    </Skeleton>
+  );
 }
 
 export default AdminPhysicianPatientAppointmentTab;
