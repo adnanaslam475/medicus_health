6a27803d12fe4fbe72ea5a08540dd1b9e4cce159
MrFarhan <muhammadfarhan012345@gmail.com>
2022-07-13 20:36:41 +0500

    Updated attachment name

--- src/common/hooks/media.ts
+++ src/common/hooks/media.ts
@@ -23,7 +23,7 @@ export const useMediaUploader = () => {
   const uploadMultiple = async (file: File[]) => {
     try {
       return await Promise.all(
-        file.map((file: File) => s3.uploadFile(file as File, file.name.split(".")[0]))
+        file.map((file: File) => s3.uploadFile(file as File, file.name))
       );
     } catch (error) {
       console.log(error);
