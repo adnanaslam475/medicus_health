7bafb27952ee7c3c3888dd079b4f98faa018d6ad
usama khan <usama.khan@cooperativecomputing.com>
2022-07-27 21:41:45 +0500

    setpush

--- src/common/components/OnlySearchFilters/OnlySearchFilters.tsx
+++ src/common/components/OnlySearchFilters/OnlySearchFilters.tsx
@@ -26,7 +26,7 @@ function OnlySearchFilters({ onChange }: Props) {
 
       <div className="lg:ml-3 w-full sm:w-full md:w-full lg:max-w-[430px] mr-2">
         <Input
-          placeholder="ID#, patient name, contact number or email"
+          placeholder="ID#, patient name or email"
           prefix={<SearchOutlined />}
           value={searchValue}
           onChange={(e) => onChangeFields(e.target.value)}
