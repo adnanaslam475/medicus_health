4b4a39d8049d71bb5ef0322863b14a2e5558b2aa
ali <ali.imtiaz@cooperativecomputing.com>
2022-08-12 17:26:01 +0500

    Query for Count

--- pages/physician/staff/DoctorStaffDetails/StaffdetailsForm.tsx
+++ pages/physician/staff/DoctorStaffDetails/StaffdetailsForm.tsx
@@ -71,7 +71,7 @@ function StaffDetailsFrom({
         </Button>
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-4">
-        <AddStaffFormItems accountCreatedAt={staffDetail?.createdAt}/>
+        <AddStaffFormItems accountCreatedAt={staffDetail?.createdAt} />
       </div>
       <div className="flex justify-end mt-2">
         <Form.Item>
