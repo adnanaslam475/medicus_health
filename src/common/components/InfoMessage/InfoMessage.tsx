651ecc52d6076b6745ab9bb0bbd41e9a67d02a2b
ali <ali.imtiaz@cooperativecomputing.com>
2022-09-01 17:38:25 +0500

    pull from master

--- src/common/components/InfoMessage/InfoMessage.tsx
+++ src/common/components/InfoMessage/InfoMessage.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 const InfoMessage = () => {
   return (
-    <div className="flex items-center bg-gray-4 p-2 lg:h-10 md:h-auto px-2 rounded text-xs text-nowr">
+    <div className="flex items-center bg-gray-4 p-2 lg:h-10 md:h-auto px-2 rounded text-xs">
       {/* <span className="mr-3 mb-0"><WarningFilled style={{ color: 'white' backgroundColor: 'red' }} /></span> */}
       <Image
         priority={true}
@@ -18,11 +18,14 @@ const InfoMessage = () => {
         width={34}
         src="/assets/icon/warning-small.svg"
       />
-      <span className="ml-3 min-h-max hidden md:block">
+      <span className="ml-3 min-h-max md:block">
         Please complete the health questionnaire in order to book appointments
         with our Physicians.
       </span>
-      <span className="ml-3 min-h-max block md:hidden">Questionnaire</span>
+      {/* <span className="ml-3 min-h-max block md:hidden">
+        Please complete the health questionnaire in order to book appointments
+        with our Physicians.
+      </span> */}
       <Link href="/patient/account?activeTab=2">
         <a className="underline text-primary px-3 whitespace-nowrap">
           Complete Now
