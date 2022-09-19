5ff49ce6cb77edfcdf8ca8b9a63742c02e1a0e03
Ali Imtiaz <barro345@gmail.com>
2022-09-19 18:23:37 +0500

    MDCS-2865 MDCS-1252 MDCS-2866

--- src/common/components/StatisticsCard/StatisticsCard.tsx
+++ src/common/components/StatisticsCard/StatisticsCard.tsx
@@ -12,19 +12,21 @@ function StatisticsCard({ src, title, value }: Props) {
     <div
       className={`bg-gray-4 border border-gray-4 flex flex-col py-20 px-4 mx-4 my-3`}
     >
-      <div className="rounded-full bg-white px-3 border border-gray-4 mx-auto p-2 shadow-md text-center">
+      <div className="rounded-full bg-white px-3 border border-gray-4 mx-auto p-2 pb-1 shadow-md text-center  ">
         <Image
           priority={true}
           unoptimized
           alt=""
-          className=""
-          height={28}
+          className="dashboardIcons m-h-[30px]"
+          height={30}
           width={28}
           src={src}
         />
       </div>
 
-      <div className="text-6xl mx-auto px-auto text-center py-2">{value}</div>
+      <div className="text-6xl mx-auto px-auto text-center py-2 font-circular">
+        {value}
+      </div>
       <div className="text-center text-xl">{title} </div>
     </div>
   );
