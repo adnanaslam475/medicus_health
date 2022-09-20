df38b447d768f0b966a23f217e9ff536c7ea0c5b
Majid <majid.mateen@cooperativecomputing.com>
2022-09-20 14:22:53 +0500

    fixed MDCS-2960 and MDCS-2967

--- src/modules/admin/pages/ProfileDetail/ProfileDetail.tsx
+++ src/modules/admin/pages/ProfileDetail/ProfileDetail.tsx
@@ -126,7 +126,7 @@ function ProfileDetail() {
               )} */}
               <AccountsProfile />
             </TabPane>
-            <TabPane
+            {/* <TabPane
               tab={
                 <span>
                   <BellOutlined />
@@ -136,7 +136,7 @@ function ProfileDetail() {
               key="2"
             >
               <EmailNotification />
-            </TabPane>
+            </TabPane> */}
             <TabPane
               tab={
                 <span>
