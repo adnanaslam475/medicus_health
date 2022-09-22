f9ba50447de9218519ffb72d29d1f33c13151ad1
Majid <majid.mateen@cooperativecomputing.com>
2022-09-22 15:40:56 +0500

    MDCS-2995 MDCS-2994

--- src/modules/admin/components/AddPatientForm/AddPatientForm.tsx
+++ src/modules/admin/components/AddPatientForm/AddPatientForm.tsx
@@ -96,12 +96,12 @@ export const AddPatientForm = React.forwardRef(function AddPhysicianForm(
           className="flex-1"
           label="Contact #"
           name="contact_number"
-          rules={[
-            {
-              required: true,
-              message: "Please enter your contact number",
-            },
-          ]}
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: "Please enter your contact number",
+          //   },
+          // ]}
         >
           <Input type="tel" />
         </Form.Item>
@@ -112,12 +112,12 @@ export const AddPatientForm = React.forwardRef(function AddPhysicianForm(
           className="flex-1"
           label="Street address"
           name="streetAddress"
-          rules={[
-            {
-              required: true,
-              message: "Please enter your street address",
-            },
-          ]}
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: "Please enter your street address",
+          //   },
+          // ]}
         >
           <Input />
         </Form.Item>
@@ -127,12 +127,12 @@ export const AddPatientForm = React.forwardRef(function AddPhysicianForm(
           className="flex-1"
           label="Country"
           name="country"
-          rules={[
-            {
-              required: true,
-              message: "Please enter your country",
-            },
-          ]}
+          // rules={[
+          //   {
+          //     required: true,
+          //     message: "Please enter your country",
+          //   },
+          // ]}
         >
           <Select
             showSearch
@@ -224,7 +224,7 @@ export const AddPatientForm = React.forwardRef(function AddPhysicianForm(
           </Select>
         </Form.Item>
 
-        <Form.Item
+        {/* <Form.Item
           className="flex-1"
           label="Postal code"
           name="postalCode"
@@ -236,7 +236,7 @@ export const AddPatientForm = React.forwardRef(function AddPhysicianForm(
           ]}
         >
           <Input type="number" />
-        </Form.Item>
+        </Form.Item> */}
       </div>
       <Form.Item>
         <div className="flex items-center justify-end">
