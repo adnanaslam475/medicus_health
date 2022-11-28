34f235f6c60ddd12c4b6bfed427d1d66000b9428
Asad Ali Khan WL1 <asad.khan@cooperativecomputing.com>
2022-11-28 12:01:19 +0500

    MDCS-3307

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -2062,7 +2062,7 @@ export type UpdateDoctorProfileMutationVariables = Exact<{
 }>;
 
 
-export type UpdateDoctorProfileMutation = { __typename?: 'Mutation', updateDoctorProfile: { __typename?: 'DoctorProfile', id: number, doctor_id: number, year_of_experience?: number | null, specialization?: string | null, condition_treated?: string | null, educational_background?: string | null, professional_experience?: string | null, certification_and_licensure?: string | null, awards_honors_recognition?: string | null, language?: any | null, about_me?: string | null, profile_image?: string | null, user?: { __typename?: 'User', id: number, first_name: string, last_name: string, email: string, gender?: string | null, streetAddress?: string | null, contact_number?: string | null, country_id?: number | null, state_id?: number | null, city_id?: number | null, timeZoneId?: number | null, zip_code?: string | null, password?: string | null, status: boolean, role?: string | null, city?: { __typename?: 'City', city_name: string } | null, state?: { __typename?: 'State', state_name: string } | null, country?: { __typename?: 'Country', country_name: string } | null } | null } };
+export type UpdateDoctorProfileMutation = { __typename?: 'Mutation', updateDoctorProfile: { __typename?: 'DoctorProfile', id: number, doctor_id: number, year_of_experience?: number | null, specialization?: string | null, condition_treated?: string | null, educational_background?: string | null, professional_experience?: string | null, certification_and_licensure?: string | null, awards_honors_recognition?: string | null, language?: any | null, about_me?: string | null, profile_image?: string | null, profile_video?: string | null, user?: { __typename?: 'User', id: number, first_name: string, last_name: string, email: string, gender?: string | null, streetAddress?: string | null, contact_number?: string | null, country_id?: number | null, state_id?: number | null, city_id?: number | null, timeZoneId?: number | null, zip_code?: string | null, password?: string | null, status: boolean, role?: string | null, city?: { __typename?: 'City', city_name: string } | null, state?: { __typename?: 'State', state_name: string } | null, country?: { __typename?: 'Country', country_name: string } | null } | null } };
 
 export type EnableOrDisableDoctorMutationVariables = Exact<{
   id: Scalars['Int'];
@@ -3174,6 +3174,7 @@ export const UpdateDoctorProfileDocument = gql`
     language
     about_me
     profile_image
+    profile_video
     user {
       id
       first_name
