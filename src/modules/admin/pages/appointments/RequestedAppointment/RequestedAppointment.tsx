ff28ffb9b32015224af1ae5a2108030efcc29a60
Asad Ali Khan WL1 <asad.khan@cooperativecomputing.com>
2022-11-30 15:27:43 +0500

    MDCS-3326 #time 6h #inCR #comment removed hello Qadir

--- src/modules/admin/pages/appointments/RequestedAppointment/RequestedAppointment.tsx
+++ src/modules/admin/pages/appointments/RequestedAppointment/RequestedAppointment.tsx
@@ -102,7 +102,6 @@ function RequestedAppointment() {
   return (
     <AppLayout>
       <>
-      <h3>hello Qadir</h3>
         <div className="w-full">
           <div className="flex-none sm:flex items-center justify-between mb-5">
             <div className="pr-3 mb-3 sm:mb-0">
@@ -138,7 +137,7 @@ function RequestedAppointment() {
               >
                 <Button
                   type="primary"
-                  className={`text-sm ${isChrome && 'antCustomBtn'}`}
+                  className={`text-sm ${isChrome && "antCustomBtn"}`}
                   onClick={showAppointmentBookingModal}
                   disabled={
                     patientHealthHistory?.patientHealthHistory ? false : true
