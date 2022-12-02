68f44f22bbbd82d8735c8a9bd72e8faa235cb045
Asad Ali Khan WL1 <asad.khan@cooperativecomputing.com>
2022-12-02 12:46:25 +0500

    MDCS-3337 #time 2h #inCR #comment [FE] Admin | payout | Patient | delete | invalid date is showing in payout after delete the patient

--- src/modules/admin/pages/PayoutReportListing/PayoutReportListing.tsx
+++ src/modules/admin/pages/PayoutReportListing/PayoutReportListing.tsx
@@ -17,7 +17,7 @@ import { addDecimaltoAmount } from "common/utils/helper";
 function PayoutReportListing() {
   const { Panel } = Collapse;
   const onChange = (key: string | string[]) => {};
-
+  console.log("date.formatDAYMMDD(scheduleDate)", date.formatDAYMMDD(" "));
   const columns = [
     {
       title: "ID#",
@@ -61,7 +61,13 @@ function PayoutReportListing() {
       key: "appointmentDateTime",
       render: (appointmentDateTime: any) => {
         const scheduleDate = appointmentDateTime?.startTime || "";
-        return <div>{date.formatDAYMMDD(scheduleDate)}</div>;
+        return (
+          <div>
+            {date.formatDAYMMDD(scheduleDate) === "Invalid Date"
+              ? "-"
+              : date.formatDAYMMDD(scheduleDate)}
+          </div>
+        );
       },
     },
     {
