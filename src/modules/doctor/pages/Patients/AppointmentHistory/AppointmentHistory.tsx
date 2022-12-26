050bc68bafc29a6b5ff03924dbfc8a70a15a0636
Akber Khan <akber.khan@cooperativecomputing.com>
2022-12-26 12:32:05 +0500

    console remove

--- src/modules/doctor/pages/Patients/AppointmentHistory/AppointmentHistory.tsx
+++ src/modules/doctor/pages/Patients/AppointmentHistory/AppointmentHistory.tsx
@@ -51,7 +51,6 @@ function AppointmentHistory() {
 
   const onChange = (...params: any) => {
     const [, , sorter] = params;
-    console.log(sorter);
     setSorting({
       order: sorter.order?.replace("end", "") || "",
       //column: sorter.order ? `user.${sorter.columnKey}` : "",
